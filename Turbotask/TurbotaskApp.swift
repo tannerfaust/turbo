c2d7baa60de60dde479db52f3f95625b348c3e71
tannerfaust <tannerwellspoken@proton.me>
2026-04-01 01:06:55 +0200

    Initial Commit

---
+++ Turbotask/TurbotaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurbotaskApp.swift
+//  Turbotask
+//
+//  Created by Tanner Fause on 01.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct TurbotaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
